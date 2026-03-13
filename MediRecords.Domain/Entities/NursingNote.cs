fad65e3745211b5f52407a716aa4182f0ed64a44
Chhatani <2480335@cognizant.com>
2026-03-13 17:28:57 +0530

    Comments added in VitalSign and NursingNote

--- MediRecords.Domain/Entities/NursingNote.cs
+++ MediRecords.Domain/Entities/NursingNote.cs
@@ -9,21 +9,22 @@ public class NursingNote
 {   
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.None)]
-    public int NursingNoteId { get; set; }
+    public int NursingNoteId { get; set; } //NursingNoteId Primary Key for Note IDs
+    
 
 
     [Required]
-    public int EncounterId { get; set; }
+    public int EncounterId { get; set; } // IDs for every Encounters
 
 
     [Column(TypeName ="VARCHAR(50)")]
-    public string Notes { get; set; }
+    public string Notes { get; set; } // Notes By the nurse.
 
 
     [Required]
     [Column(TypeName ="VARCHAR(50)")]
-    public string RecordedBy { get; set; }
+    public string RecordedBy { get; set; } // Name of the person who recorded this note 
 
 
-    public DateTime RecordedDate { get; set; }
+    public DateTime RecordedDate { get; set; } // Date of the Note record
 }
