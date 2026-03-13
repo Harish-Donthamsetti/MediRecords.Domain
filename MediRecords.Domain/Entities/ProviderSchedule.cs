1747abd3194c45db514d2df96851ff6d483068a6
Manan Daxini <MananChhotalal.Daxini@cognizant.com>
2026-03-13 17:20:11 +0530

    Added Entity Class For Appointment and ProviderSchedule

---
+++ MediRecords.Domain/Entities/ProviderSchedule.cs
@@ -0,0 +1,34 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MediRecords.Domain.Entities;
+
+[Table("ProviderSchedule")]
+public class ProviderSchedule
+{
+    [Key]
+    public int ScheduleId { get; set; }
+
+    [Required]
+    public int ProviderId { get; set; }
+
+    [Required]
+    [MaxLength(10)]
+    [Column(TypeName = "varchar(10)")]
+    public string DayOfWeek { get; set; } = string.Empty;
+
+    [Required]
+    [Column(TypeName = "time")]
+    public DateTime StartTime { get; set; }
+
+    [Required]
+    [Column(TypeName = "time")]
+    public DateTime EndTime { get; set; }
+
+    [Required]
+    public int SlotDuration { get; set; }
+
+    [Required]
+    public bool Status { get; set; }
+}
