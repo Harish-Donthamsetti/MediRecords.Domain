0414197ad309e1f842a198ececaa14c005a8bb22
Chhatani <2480335@cognizant.com>
2026-03-13 17:28:57 +0530

    Comments added in VitalSign and NursingNote

--- MediRecords.Domain/Entities/VitalSign.cs
+++ MediRecords.Domain/Entities/VitalSign.cs
@@ -10,31 +10,31 @@ public class VitalSign
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.None)]
-    public int VitalId { get; set; }
+    public int VitalId { get; set; } // Primary Key ID for all Vital Signs.
     
 
-    public int EncounterId { get; set; }
+    public int EncounterId { get; set; } // ID of the Encounter.
 
 
     [Required]
     [Column(TypeName="VARCHAR(50)")]
-    public string Type { get; set; }
+    public string Type { get; set; } // Type of Vitals recorded.
 
 
     [Required]
     [Column(TypeName="VARCHAR(50)")]
-    public int Value { get; set; }
+    public int Value { get; set; } // Value of the Vitals recorded.
 
 
     [Required]
     [Column(TypeName="VARCHAR(20)")]
-    public string Unit{ get; set; }
+    public string Unit{ get; set; } // Unit in which the Vitals are recorded.
     
     
-    public DateTime RecordedDate { get; set; }
+    public DateTime RecordedDate { get; set; } // Date of the Vitals recorded.
 
 
     [Required]
     [Column(TypeName="VARCHAR(50)")]
-    public string RecordedBy { get; set; }
+    public string RecordedBy { get; set; } // Name of the User who recorded the Vitals.
 }
