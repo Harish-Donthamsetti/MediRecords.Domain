d55b6eed201dba44aaf2cf915a6ceb806a0728d5
shivam26ce30 <Shivam.Kumar26ce30@cognizant.com>
2026-03-16 12:23:03 +0530

    code for ProcedureCode and VisitChargeRef added

---
+++ MediRecords.Domain/Entities/ProcedureCode.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MediRecords.Domain.Entities;
+
+[Table("ProcedureCode")]
+public class ProcedureCode
+{
+    [Key]
+    public int CodeId { get; set; }
+
+    [Column(TypeName = "VARCHAR(20)")]
+    public string Code { get; set; }
+
+    [Column(TypeName = "VARCHAR(255)")]
+    public string Description { get; set; }
+
+    [Column(TypeName = "DECIMAL(18,2)")]
+    public decimal Price { get; set; }
+
+    public virtual ICollection<VisitChargeRef> VisitCharges { get; set; } = new List<VisitChargeRef>();
+}
